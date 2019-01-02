84a9823202d52984160016b17c7c9f9388d18c58
mtw2017 <jilufe@gmail.com>
2019-01-02 22:46:47 +1300

    correct getGeners function in genreService file

--- src/services/genreService.jsx
+++ src/services/genreService.jsx
@@ -1,6 +1,8 @@
 import http from "./httpService";
 import config from "./../config.json";
 
+const apiEndpoint = "/genres";
+
 export function getGenres() {
-  return http.get(config + "/genres");
+  return http.get(apiEndpoint);
 }
--- src/services/movieService.jsx
+++ src/services/movieService.jsx
@@ -6,21 +6,6 @@ export function getMovies() {
   return http.get(apiEndpoint);
 }
 
-// export function saveMovie(movie) {
-//   let movieInDb = movies.find(m => m._id === movie._id) || {};
-//   movieInDb.title = movie.title;
-//   movieInDb.genre = generesAPI.genres.find(g => g._id === movie.genreId);
-//   movieInDb.numberInStock = movie.numberInStock;
-//   movieInDb.dailyRentalRate = movie.dailyRentalRate;
-
-//   if (!movieInDb._id) {
-//     movieInDb._id = Date.now().toString();
-//     movies.push(movieInDb);
-//   }
-
-//   return movieInDb;
-// }
-
 function movieUrl(id) {
   return `${apiEndpoint}/${id}`;
 }
