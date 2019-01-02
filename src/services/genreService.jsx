55223ae6ff0fd0e1c05f3aa964236df644c626d3
mtw2017 <jilufe@gmail.com>
2019-01-02 18:04:17 +1300

    change

--- src/services/genreService.jsx
+++ src/services/genreService.jsx
@@ -2,5 +2,5 @@ import http from "./httpService";
 import config from "./../config.json";
 
 export function getGenres() {
-  return http.get(config.apiUrl + "/genres");
+  return http.get(config + "/genres");
 }
